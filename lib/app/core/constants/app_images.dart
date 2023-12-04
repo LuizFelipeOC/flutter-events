2359ccbec6d5440206d80356b140b4fc69493d46
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2023-12-04 15:12:56 -0400

    feat: :sparkles: add path of image to background card events

--- lib/app/core/constants/app_images.dart
+++ lib/app/core/constants/app_images.dart
@@ -3,4 +3,5 @@ final class AppImages {
   static String get dashStars1 => 'assets/images/dash1.png';
   static String get dashStars2 => 'assets/images/dash2.png';
   static String get dashStars3 => 'assets/images/dash3.png';
+  static String get mockCard => 'assets/images/mock_card.png';
 }
