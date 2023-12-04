3c38a57fb2feb230a8277a34ef7efd23b95ab7fc
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2023-12-04 10:45:33 -0400

    chore: :truck: move my app widget

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'features/splash/view/splash_page.dart';
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Events',
+      home: SplashPage(),
     );
   }
 }
