0b14f34d95a498479ddf83bb26a57a44c50d9698
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2023-12-04 15:28:03 -0400

    refactor: :recycle: add icon to open menu

--- lib/app/features/home/views/widgets/home_app_bar.dart
+++ lib/app/features/home/views/widgets/home_app_bar.dart
@@ -11,25 +11,41 @@ class HomeAppBar extends PreferredSize {
               padding: const EdgeInsets.symmetric(horizontal: 20),
               alignment: Alignment.centerLeft,
               height: 140,
-              child: Text.rich(
-                TextSpan(
-                  text: 'Olá!\n',
-                  style: const TextStyle(
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 22,
-                  ),
-                  children: [
-                    TextSpan(
-                      text: 'Aprenda as últimas novidades do Flutter e se divirta muito!',
-                      style: TextStyle(
-                        color: Colors.grey.shade600,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 16,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 50,
+                    alignment: Alignment.topRight,
+                    child: CircleAvatar(
+                      backgroundColor: Colors.grey.shade200,
+                      child: Icon(
+                        Icons.person_outline_rounded,
+                        color: Colors.grey.shade500,
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  Text.rich(
+                    TextSpan(
+                      text: 'Olá!\n',
+                      style: const TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: 'Aprenda as últimas novidades do Flutter e se divirta muito!',
+                          style: TextStyle(
+                            color: Colors.grey.shade600,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
