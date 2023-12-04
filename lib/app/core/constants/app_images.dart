cb65ada05980bdf056940917cb641f6971e0b949
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2023-12-04 11:56:53 -0400

    feat: :sparkles: add paths to dashes images to onboarding page

--- lib/app/core/constants/app_images.dart
+++ lib/app/core/constants/app_images.dart
@@ -1,3 +1,6 @@
 final class AppImages {
   static String get dashStars => 'assets/images/dashatars.png';
+  static String get dashStars1 => 'assets/images/dash1.png';
+  static String get dashStars2 => 'assets/images/dash2.png';
+  static String get dashStars3 => 'assets/images/dash3.png';
 }
